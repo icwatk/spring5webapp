a5dd9ef572dacb7e9a76d81e80e009b4bf30b1dd
icw11 <icw1190@gmail.com>
2022-05-28 13:11:11 -0400

    added repositories and bootstrapdata class

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,23 +1,28 @@
 package guru.springframework.spring5webapp.domain;
 
+import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 public class Author {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String firstName;
     private String lastName;
-    private Set<Book> bookSet;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books = new HashSet<>();
 
     public Author() {
     }
 
-    public Author(String firstName, String lastName, Set<Book> bookSet) {
+    public Author(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.bookSet = bookSet;
     }
-
-
 
 
     public String getFirstName() {
@@ -36,11 +41,36 @@ public class Author {
         this.lastName = lastName;
     }
 
-    public Set<Book> getBookSet() {
-        return bookSet;
+    public Set<Book> getBooks() {
+        return books;
     }
 
-    public void setBookSet(Set<Book> bookSet) {
-        this.bookSet = bookSet;
+    public void setBooks(Set<Book> bookSet) {
+        this.books = bookSet;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id != null ? id.equals(author.id) : author.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,17 +1,31 @@
 package guru.springframework.spring5webapp.domain;
 
+import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 public class Book {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String title;
     private String isbn;
-    private Set<Author> authorSet;
 
-    public Book(String title, String isbn, Set<Author> authorSet) {
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+    inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors = new HashSet<>();
+
+    public Book(String title, String isbn) {
         this.title = title;
         this.isbn = isbn;
-        this.authorSet = authorSet;
+
+    }
+
+    public Book() {
     }
 
     public String getTitle() {
@@ -30,11 +44,36 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public Set<Author> getAuthorSet() {
-        return authorSet;
+    public Set<Author> getAuthors() {
+        return authors;
     }
 
-    public void setAuthorSet(Set<Author> authorSet) {
-        this.authorSet = authorSet;
+    public void setAuthors(Set<Author> authorSet) {
+        this.authors = authorSet;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return id != null ? id.equals(book.id) : book.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
